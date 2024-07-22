c6ccb688282ff95997c41f8b2b64f4e13082aa83
Snowden-0 <uurrehman735@gmail.com>
2024-07-22 13:05:54 +0500

    changed copy cutton color to black

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ function App() {
 
   return (
     <>
-      <div className='w-full max-w-md mx-auto shadow-2xl rounded-lg px-4 py-6 my-16 bg-gray-700'>
+      <div className='w-full max-w-md mx-auto shadow-2xl rounded-lg px-4 py-6 my-16 bg-gray-800'>
         <h1 className='text-white text-center my-5 text-2xl font-extrabold'>Password Generator</h1>
           <div className='flex shadow-xl rounded-lg overflow-hidden mb-6'> 
             <input 
@@ -49,7 +49,7 @@ function App() {
             />
             <button 
             onClick={copyPasswordToClipboard}
-            className='outline-none bg-blue-700 text-white px-3 py-2 shrink-0'>Copy</button>
+            className='outline-none bg-black text-white px-3 py-2 shrink-0'>Copy</button>
         </div>
         <div className='flex text-sm gap-x-2'>
           <div className='flex items-center gap-x-1'>
@@ -58,7 +58,7 @@ function App() {
             min={6}
             max={100}
             value={length}
-            className='cursor-pointer'
+            className='cursor-pointer '
             onChange={(e) => setLength(e.target.value)}
             />
               <label className='text-orange-500 font-bold'>Length: {length}</label>
